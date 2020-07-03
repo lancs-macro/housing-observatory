0d1afb0064350dc796f2eae04786e0fbd2f5ab29
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2020-07-03 20:16:38 +0100

    update to release 2020-Q1

--- assets/js/stat.js
+++ assets/js/stat.js
@@ -1,6 +1,6 @@
-var txt = '{"release":["2019 Q4"],"price_uk":[-0.6],"price_london":[-3.26],"afford_uk":[-0.23],"afford_london":[-3.09]}';
+var txt = '{"release":["2020 Q1"],"price_uk":[0.73],"price_london":[-0.71],"afford_uk":[0.43],"afford_london":[-0.15]}';
 var obj = JSON.parse(txt);
-document.getElementById("js-release").innerHTML = obj.release;
+document.getElementById("js-release").innerHTML = "Release: <br>" + obj.release;
 document.getElementById("js-price-uk").innerHTML = obj.price_uk + " %";
 document.getElementById("js-price-london").innerHTML = obj.price_london + " %";
 document.getElementById("js-afford-uk").innerHTML = obj.afford_uk + " %";
